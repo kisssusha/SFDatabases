d39cf9c1e2997d9c281561178681fa73d125b192
Ksenia Kolpikova <89912964+kisssusha@users.noreply.github.com>
2025-03-18 17:39:12 +0300

    Update Task3_2.sql

--- task3/Task3_2.sql
+++ task3/Task3_2.sql
@@ -1,14 +1,30 @@
-SELECT c.ID_customer                                              AS ID_customer,
-       c.name                                                     AS name,
-       COUNT(b.ID_booking)                                        AS total_bookings,
-       SUM(r.price * DATEDIFF(b.check_out_date, b.check_in_date)) AS total_spent,
-       COUNT(DISTINCT h.ID_hotel)                                 AS unique_hotels
-FROM Customer c
-         JOIN Booking b ON c.ID_customer = b.ID_customer
-         JOIN Room r ON b.ID_room = r.ID_room
-         JOIN Hotel h ON r.ID_hotel = h.ID_hotel
-GROUP BY c.ID_customer
-HAVING COUNT(DISTINCT h.ID_hotel) > 1
-   AND COUNT(b.ID_booking) > 2
-   and total_spent > 500
-ORDER BY total_spent ASC;
+WITH ClientBookings AS (SELECT c.ID_customer,
+                               c.name,
+                               COUNT(b.ID_booking)        AS total_bookings,
+                               COUNT(DISTINCT h.ID_hotel) AS unique_hotels,
+                               SUM(r.price)               AS total_spent
+                        FROM Booking b
+                                 JOIN Customer c ON b.ID_customer = c.ID_customer
+                                 JOIN Room r ON b.ID_room = r.ID_room
+                                 JOIN Hotel h ON r.ID_hotel = h.ID_hotel
+                        GROUP BY c.ID_customer, c.name
+                        HAVING COUNT(b.ID_booking) > 2
+                           AND COUNT(DISTINCT h.ID_hotel) > 1),
+     ClientSpentMoreThan500 AS (SELECT c.ID_customer,
+                                       c.name,
+                                       SUM(r.price)        AS total_spent,
+                                       COUNT(b.ID_booking) AS total_bookings
+                                FROM Booking b
+                                         JOIN Customer c ON b.ID_customer = c.ID_customer
+                                         JOIN Room r ON b.ID_room = r.ID_room
+                                GROUP BY c.ID_customer, c.name
+                                HAVING SUM(r.price) > 500)
+
+SELECT cb.ID_customer,
+       cb.name,
+       cb.total_bookings,
+       cb.total_spent,
+       cb.unique_hotels
+FROM ClientBookings cb
+         JOIN ClientSpentMoreThan500 csm ON cb.ID_customer = csm.ID_customer
+ORDER BY cb.total_spent ASC;
